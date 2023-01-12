892ffd7e9f61d67d24fe1e4dc768ae60802cd5e3
Charles Boudry <chboudry@outlook.com>
2023-01-12 09:18:52 +0100

    Update main.tf

--- Terraform single module on different subscriptions/main.tf
+++ Terraform single module on different subscriptions/main.tf
@@ -10,11 +10,11 @@ terraform {
 provider "azurerm" {
   features {}
   alias = "sub1"
-  subscription_id = "dc8988ff-b13d-4b92-b3f6-af40b06b9be8"
+  subscription_id = "XXXXXXX"
 }
 
 provider "azurerm" {
   features {}  
   alias           = "sub2"
-  subscription_id = "320c35fb-03ca-42b2-a67d-46a71839aad1"
+  subscription_id = "XXXXXXXX"
 }
